ebacf76110c3bebad49bee7b456ac41f04edebcf
Sebas Castillo <sebascastillo@gmail.com>
2016-05-26 18:14:55 +0200

    Primera subida

---
+++ qarq/src/main/java/com/softtek/QArqApplication.java
@@ -0,0 +1,12 @@
+package com.softtek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QArqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QArqApplication.class, args);
+	}
+}
---
+++ qarq/src/test/java/com/softtek/QArqApplicationTests.java
@@ -0,0 +1,18 @@
+package com.softtek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QArqApplication.class)
+@WebAppConfiguration
+public class QArqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
